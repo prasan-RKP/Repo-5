686188ffe3638b14e7220490ff596317073d707b
Prasan RKP <prasan543e@gmail.com>
2025-06-15 11:55:07 +0530

    main completed

---
+++ script.js
@@ -0,0 +1,6 @@
+const incBtn = document.getElementById("inc");
+const decBtn = document.getElementById("dec");
+const countText = document.getElementById("count");
+
+let countVal = parseInt(countText.innerText);
+
