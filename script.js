59fd5d196cf6eb656c6662f1385dea653b745b48
Prasan RKP <prasan543e@gmail.com>
2025-06-18 22:40:14 +0530

    async opt added

--- script.js
+++ script.js
@@ -37,3 +37,14 @@ myPromise.then(
   function(value) { /* code if successful */ },
   function(error) { /* code if some error */ }
 );
+
+
+
+async function myDisplay() {
+  let myPromise = new Promise(function(resolve, reject) {
+    resolve("I love You !!");
+  });
+  document.getElementById("demo").innerHTML = await myPromise;
+}
+
+myDisplay();
