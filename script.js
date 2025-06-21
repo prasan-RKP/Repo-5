2c73aa44990b09389d291b2153ca06f5151478ef
Prasan RKP <prasan543e@gmail.com>
2025-06-21 23:38:55 +0530

    f/h added

--- script.js
+++ script.js
@@ -50,3 +50,13 @@ async function myDisplay() {
 
 myDisplay();
 
+
+// program to convert celsius to fahrenheit
+// ask the celsius value to the user 
+const celsius = prompt("Enter a celsius value: ");
+
+// calculate fahrenheit
+const fahrenheit = (celsius * 1.8) + 32
+
+// display the result
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
