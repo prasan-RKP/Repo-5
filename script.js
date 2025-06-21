d3ad197ac59113b39e807adf77a1652d3b99c6f4
Prasan RKP <prasan543e@gmail.com>
2025-06-21 23:33:13 +0530

    deleted few codes

--- script.js
+++ script.js
@@ -50,13 +50,3 @@ async function myDisplay() {
 
 myDisplay();
 
-//  Random value between two numbers which is taken by User Prompt(user Input)
-// input from the user
-const min = parseInt(prompt("Enter a min value: "));
-const max = parseInt(prompt("Enter a max value: "));
-
-// generating a random number
-const a = Math.floor(Math.random() * (max - min + 1)) + min;
-
-// display a random number
-console.log(`Random value between ${min} and ${max} is ${a}`);
